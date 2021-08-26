57a7c3c57460947a84ebd652c2fc1170e5e1077d
EugeneJoe <jmuthui70@gmail.com>
2021-08-26 09:05:35 +0000

    Update

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,12 +9,6 @@ import os
 from datetime import datetime
 from fabric.api import local
 
-#env.hosts = [
-#    '34.139.119.72',
-#    '34.139.135.201'
-#]
-#env.user = 'ubuntu'
-
 
 def do_pack():
     """ Creates a .tgz archive from the contents of the web_static folder """
@@ -22,11 +16,10 @@ def do_pack():
     now_str = now.strftime("%Y%m%d%H%M%S")
     now_str = now_str.replace('/', '')
     if not os.path.exists('versions'):
-        os.makedirs('versions')
+        local("mkdir versions")
     tar_name = "versions/web_static_{}.tgz".format(now_str)
-    print("Packing web_static to {}".format(tar_name))
     result = local("tar -cvzf {} web_static".format(tar_name))
     if result.succeeded:
-        return tar_name
+        tar_name
     else:
         return None
