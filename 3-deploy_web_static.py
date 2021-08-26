4acf3e5b204958faeb92357bb64f38cf70ad2503
EugeneJoe <jmuthui70@gmail.com>
2021-08-26 23:38:27 +0000

    Use sudo

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -39,13 +39,13 @@ def do_deploy(archive_path):
         rempath = "/data/web_static/releases/"
 
         put(archive_path, "/tmp/".format(localpath))
-        run("mkdir -p {}{}".format(rempath, newpath))
-        run("tar -xzf /tmp/{} -C {}{}".format(localpath, rempath, newpath))
-        run("rm /tmp/{}".format(localpath))
-        run("mv {0}{1}/web_static/* {0}{1}/".format(rempath, newpath))
-        run("rm -rf {}{}/web_static".format(rempath, newpath))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(rempath, newpath))
+        sudo("mkdir -p {}{}".format(rempath, newpath))
+        sudo("tar -xzf /tmp/{} -C {}{}".format(localpath, rempath, newpath))
+        sudo("rm /tmp/{}".format(localpath))
+        sudo("cp -r {0}{1}/web_static/* {0}{1}/".format(rempath, newpath))
+        sudo("rm -rf {}{}/web_static".format(rempath, newpath))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s {}{}/ /data/web_static/current".format(rempath, newpath))
         return True
     except:
         return False
