7e6bb5fb9297f4657672052cf79545edf75252ca
EugeneJoe <jmuthui70@gmail.com>
2021-08-27 01:43:54 +0000

    Update

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -12,21 +12,12 @@ $var="server {
         listen 80 default_server;
         listen [::]:80 default_server;
 
-	root /var/www/html;
-	index index.html index.htm;
-
         location /hbnb_static {
             alias /data/web_static/current;
 	    index index.html index.htm;
         }
 
         add_header X-Served-By ${hostname};
-
-	error_page 404 /404.html;
-	location /404 {
-           root /var/www/html;
-	   internal;
-	}
 
         rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGU1\wu4 permanent;
 }"
@@ -77,24 +68,6 @@ exec { 'permissions' :
   path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin', 'usr/local/bin'],
   command => 'chown -R ubuntu:ubuntu /data/',
   returns => [0,1],
-} ->
-
-file { '/var/www':
-  ensure => 'directory'
-} ->
-
-file { '/var/www/html':
-  ensure => 'directory'
-} ->
-
-file { '/var/www/html/index.html':
-  ensure  => 'present',
-  content => "Holberton School\n"
-} ->
-
-file { '/var/www/html/404.html':
-  ensure  => 'present',
-  content => "Ceci n'est pas une page\n"
 } ->
 
 file { '/etc/nginx/sites-available/default':
