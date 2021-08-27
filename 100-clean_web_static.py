166c85e668724129a1b84b6a189611d8f3b81336
EugeneJoe <jmuthui70@gmail.com>
2021-08-27 02:24:14 +0000

    Fix pep8 errors

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-# Deletes out-of-date archives
+""" Deletes out-of-date archives """
 
 from os import listdir
 from fabric.api import run, local, cd, lcd, env
 
 env.hosts = ['34.139.119.72', '34.139.135.201']
+
 
 def do_clean(number=0):
     """ Deletes out-of-date archives locally and on my remote hosts """
