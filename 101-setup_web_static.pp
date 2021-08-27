39e57af3a6d386b3e5f4dd54c7512d7d6e4ed947
EugeneJoe <jmuthui70@gmail.com>
2021-08-27 00:31:39 +0000

    Update

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,36 +1,5 @@
 # Set up a web server for deployment of web files
 
-exec { 'Update':
-  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command  => 'sudo apt-get update -y',
-  provider => 'shell',
-  returns  => [0,1],
-}
-
-package { 'nginx':
-  ensure => 'present',
-}
-
-file { '/data':
-  ensure  => 'directory',
-}
-
-file { '/data/web_static':
-  ensure => 'directory',
-}
-
-file { '/data/web_static/releases':
-  ensure => 'directory',
-}
-
-file { '/data/web_static/releases/test':
-  ensure => 'directory',
-}
-
-file { '/data/web_static/shared':
-  ensure => 'directory',
-}
-
 $html = "<html>
   <head>
   </head>
@@ -38,24 +7,6 @@ $html = "<html>
     Holberton School
   </body>
 </html>"
-
-file { '/data/web_static/releases/test/index.html':
-  ensure  => file,
-  content => $html,
-}
-
-file { '/data/web_static/current':
-  ensure => 'link',
-  target => '/data/web_static/releases/data/',
-}
-
-exec { 'permissions':
-  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin', 'usr/local/bin'],
-  command  => 'chown -R ubuntu:ubuntu /data/',
-  provider => 'shell',
-  returns  => [0,1],
-  require  => Exec['/data/'],
-}
 
 $var="server {
         listen 80 default_server;
@@ -68,10 +19,59 @@ $var="server {
 permanent;
 }"
 
+exec { 'Update':
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  => 'sudo apt-get update -y',
+  provider => 'shell',
+  returns  => [0,1],
+} ->
+
+package { 'nginx':
+  ensure => 'present',
+} ->
+
+file { '/data':
+  ensure  => 'directory',
+} ->
+
+file { '/data/web_static':
+  ensure => 'directory',
+} ->
+
+file { '/data/web_static/releases':
+  ensure => 'directory',
+} ->
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory',
+} ->
+
+file { '/data/web_static/shared':
+  ensure => 'directory',
+} ->
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => file,
+  content => $html,
+} ->
+
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/data/',
+} ->
+
+exec { 'permissions':
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin', 'usr/local/bin'],
+  command  => 'chown -R ubuntu:ubuntu /data/',
+  provider => 'shell',
+  returns  => [0,1],
+  require  => Exec['/data/'],
+} ->
+
 file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
   content => $var,
-}
+} ->
 
 exec { 'start_server':
   require  => Exec['configuration'],
