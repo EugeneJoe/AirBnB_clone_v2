2d854e10b6493c22c559a14d8a8af6d4567a4e24
EugeneJoe <jmuthui70@gmail.com>
2021-08-27 12:15:03 +0000

    Use sudo

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Deletes out-of-date archives """
 from os import listdir
-from fabric.api import run, local, cd, lcd, env
+from fabric.api import sudo, local, cd, lcd, env, run
 
 env.hosts = ['34.139.119.72', '34.139.135.201']
 
@@ -23,4 +23,4 @@ def do_clean(number=0):
         files = run("ls -tr").split()
         old_new = [a for a in files if "web_static_" in a]
         [old_new.pop() for i in range(number) if old_new]
-        [run("rm -rf ./{}".format(arch)) for arch in old_new]
+        [sudo("rm -rf ./{}".format(arch)) for arch in old_new]
